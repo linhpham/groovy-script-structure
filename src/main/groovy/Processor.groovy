c00184acf213d46d953add12fdceb44432e7dcf9
Linh Pham <pd.linh@gmail.com>
2015-08-09 23:28:18 +1000

    Added logic for host - default to localhost if not provided.

--- src/main/groovy/Processor.groovy
+++ src/main/groovy/Processor.groovy
@@ -5,13 +5,21 @@ import groovy.util.logging.*
 @Log4j
 class Processor {
     def opts
-    
+
+    def host
+
     Processor(opts) {
         log.level = Level.DEBUG
         this.opts = opts
+        this.host = opts.h ?: 'localhost'
     }
 
     void run() {
-        log.info "Running..."
+        log.info "Host               : $host"
+        log.info "User               : ${opts.u}"
+        log.info "Password           : ${opts.p}"
+        log.info "Source schema      : ${opts.s}"
+        log.info "Destination schema : ${opts.d}"
+
     }
 }
--- src/test/groovy/ProcessorSpec.groovy
+++ src/test/groovy/ProcessorSpec.groovy
@@ -1,11 +1,9 @@
 import spock.lang.*
 
 class ProcessSpec extends Specification {
-    def "#first test"() {
-        when:
-        def a = true
-
-        then:
-        a == false
+    def "#new set host to parameter, or default to localhost"() {
+        expect:
+        new Processor([]).host == 'localhost'
+        new Processor([h: 'myserver']).host == 'myserver'
     }
 }
